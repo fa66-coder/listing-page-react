0505b64894577b8f206c3e92ca1f4d69c3c96527
Farhana Anjum <farhana.anjum@factset.com>
2023-04-25 23:06:53 +0530

    formatting

--- src/components/ImageGrid/ImageGridItem.jsx
+++ src/components/ImageGrid/ImageGridItem.jsx
@@ -5,9 +5,9 @@ export default function ImageGridItem({posterImage, posterName}) {
     const posterClass = posterImage.slice(0,posterImage.indexOf('.'))
     // let imgClasses = `grid-images ${posterClass}`
     return(
-    <React.Fragment>
-        <img src={`src/images/${posterImage}`} className={`grid-images ${posterClass}`} alt='poster 1 image' loading="lazy"/>
+    <div className='grid-item'  >
+     <img src={`src/images/${posterImage}`} className={`grid-images ${posterClass}`} alt='poster 1 image' loading="lazy"/>
          <div> {posterName} </div>
-    </React.Fragment>
+         </div>   
     )
 }
