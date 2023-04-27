ec6badbe2232cd09d1f44dce7092baea1e4b8c30
Farhana Anjum <farhana.anjum@factset.com>
2023-04-28 00:15:28 +0530

    adding search input

--- src/components/Dashboard/DashboardToolBar.jsx
+++ src/components/Dashboard/DashboardToolBar.jsx
@@ -8,17 +8,28 @@ import './DashboardToolBar.css'
 export default function DashboarToolBar() {
     let [showSearch, setShowSearch] = useState(true)
     let [showBackButton, setShowBackButton] = useState(true)
+    let [showSearchInput, setShowSearchInput] = useState(false)
 
     function onBackButtonClick() {
         setShowSearch(false)
         setShowBackButton(false)
+        setShowSearchInput(false)
+    }
+
+    function onSearchClick() {
+        setShowSearchInput(!showSearchInput)
+    }
+
+    function onFilterText() {
+
     }
 
     return (
         <div className='toolbar-container' style={{ backgroundImage:`url('src/images/nav_bar.png')` }}>
             {showBackButton && <img src='src/images/Back.png' className='navImages backIcon' alt='back icon' onClick={onBackButtonClick}/>}
             <h3> Romantic Comedy </h3>
-            {showSearch && <img src='src/images/search.png' className='navImages searchIcon' alt='search image'/>}
+            {showSearchInput && <input type='text' class="searchInput" onChange={onFilterText}/>}
+            {showSearch && <img src='src/images/search.png' className={`navImages searchIcon ${!showSearchInput ? 'apply-margin' : ''}`} alt='search image'  onClick={onSearchClick}/>}
         </div>
     )
 }
