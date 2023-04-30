00f36743516aa8439480f16834fe36bd4fc04d4e
Farhana Anjum <farhana.anjum@factset.com>
2023-04-30 11:30:45 +0530

    clean up

--- src/components/ImageGrid/ImageGridItem.jsx
+++ src/components/ImageGrid/ImageGridItem.jsx
@@ -1,9 +1,8 @@
 import './ImageGridLayout.css'
-import React, { Component } from 'react'
+import React from 'react'
 
 export default function ImageGridItem({posterImage, posterName}) {
     const posterClass = posterImage.slice(0,posterImage.indexOf('.'))
-    // let imgClasses = `grid-images ${posterClass}`
     return(
     <div className='grid-item'  >
      <img src={`src/images/${posterImage}`} className={`grid-images ${posterClass}`} alt='poster 1 image' loading="lazy"/>
