e81141b8591b0af5981497764758760dadf7575e
Farhana Anjum <farhana.anjum@factset.com>
2023-05-01 01:08:49 +0530

    test

--- src/components/Dashboard/DashboardToolBar.jsx
+++ src/components/Dashboard/DashboardToolBar.jsx
@@ -27,10 +27,10 @@ export default function DashboarToolBar({ onInputUpdate }) {
 
     return (
         <div className='toolbar-container' style={{ backgroundImage: `url('src/images/nav_bar.png')` }}>
-            {showBackButton && <img src='/src/images/Back.png' className='navImages backIcon' alt='back icon' onClick={onBackButtonClick} />}
+            {showBackButton && <img src='../../src/images/Back.png' className='navImages backIcon' alt='back icon' onClick={onBackButtonClick} />}
             <h3> Romantic Comedy </h3>
             {showSearchInput && <input type='text' class="searchInput" onChange={onFilterText} />}
-            {showSearch && <img src='/src/images/search.png' className={`navImages searchIcon ${!showSearchInput ? 'apply-margin' : ''}`} alt='search image' onClick={onSearchClick} />}
+            {showSearch && <img src='../../src/images/search.png' className={`navImages searchIcon ${!showSearchInput ? 'apply-margin' : ''}`} alt='search image' onClick={onSearchClick} />}
         </div>
     )
 }
